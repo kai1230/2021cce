505b092c77b3c064e03e721b7158b8098ebea5ef
許庭愷 <91170807+kai1230@users.noreply.github.com>
2021-10-13 11:59:39 +0800

    Add files via upload

---
+++ week05-3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    if(10>0)printf("10>0成立");
+    if(10<0)printf("10<0成立");
+    if(-3)printf("-3是成立的嗎?\n");
+    if(-2)printf("-2是成立的嗎?\n");
+    if(-1)printf("-1是成立的嗎?\n");
+    if(0)printf("1是成立的嗎?\n");
+    if(1)printf("1是成立的嗎?\n");
+    if(2)printf("2是成立的嗎?\n");
+    if(3)printf("3是成立的嗎?\n");
+}
