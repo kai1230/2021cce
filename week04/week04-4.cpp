3febf839b76cf67fcee16095877a04bf79996060
許庭愷 <91170807+kai1230@users.noreply.github.com>
2022-01-05 11:43:11 +0800

    Add files via upload

---
+++ week04/week04-4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=90)printf("A+");
+    else(n>=80)printf("A");
+    else(n>70)printf("B");
+    else(n>=60)printf("C");
+    else printf("D");
+
+
+
+}
